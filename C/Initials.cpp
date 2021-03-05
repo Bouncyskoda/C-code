d0b10c6895246c8f6aae73a77fff58ad090a1e2c
Bouncy Skoda <diamondminecraftadam638@gmail.com>
2021-03-05 13:47:47 -0800

    AA

---
+++ C/Initials.cpp
@@ -0,0 +1,22 @@
+// g++ initials.cpp -o ABS.o
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "  A	      BBBBB	          SSSSS \n";
+	cout << " A A        B	    B        S	  \n";
+	cout <<" A   A       B       B      S       \n";
+	cout <<"A	  A      B      B      S        \n";
+	cout <<"AAAAAAA      B     B        S        \n";
+	cout <<"A     A      B   B           S        \n";
+	cout <<"A     A      B B              S        \n";
+	cout <<"              B               S         \n";
+	cout <<"             B B             S           \n";
+	cout <<"             B   B          S             \n";
+	cout <<"             B    B        S               \n";
+	cout <<"             B     B                        \n";
+	cout <<"             B      B                        \n";
+	cout <<"             BBBBBBBB                         \n";
+
+	return 0;
+}
